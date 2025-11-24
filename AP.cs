eee33acdf3a4b869b18975855f6bf14e40a0e667
ElenaCoton <elkoton@mail.ru>
2025-11-24 13:19:23 +0300

    Add files via upload
    Арифметическая прогрессия

---
+++ AP.cs
@@ -0,0 +1,35 @@
+﻿namespace ArithProgression
+{
+    internal class AP
+    {
+        public class ArithProgr
+        {
+            private long firstNum;
+            private long step;
+
+            public void Create(long firstNum, long step)
+            {
+                this.firstNum = firstNum;
+                this.step = step;
+            }
+            public long GetNNumber(long n)
+            {
+                return firstNum + (n - 1) * step;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            long a, d,n;
+            Console.WriteLine("Введите первый член арифметической прогрессии:");
+            a = long.Parse(Console.ReadLine());
+            Console.WriteLine("Введите разность арифметической прогрессии:");
+            d = long.Parse(Console.ReadLine());
+            ArithProgr ap = new ArithProgr();
+            ap.Create(a, d);
+            Console.WriteLine("Укажите какой-нибудь номер члена арифметической прогрессии:");
+            n = long.Parse(Console.ReadLine());
+            Console.WriteLine($"Значение {n}-го члена арифметической прогрессии = {ap.GetNNumber(n)}");
+        }
+    }
+}

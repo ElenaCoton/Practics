fceaaddf962daf0ca8451f868fbf14ab2a951727
ElenaCoton <elkoton@mail.ru>
2026-02-08 19:32:17 +0300

    Add files via upload
    Упражнение 5 (контрольное). Расчет функции

---
+++ C#-2/Lab4/FuncRange/FuncRange/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FuncRange
+{
+    public partial class Form1 : Form
+    {
+        List<string> its = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NewParams newParams = new NewParams();
+            if (newParams.ShowDialog() == DialogResult.OK) 
+            {
+                labelInfo.Text = "Интервал " + newParams.XFrom + " " + newParams.XTo;
+                its.Add(Math.Sin(newParams.XFrom).ToString() + "  " + Math.Sin(newParams.XTo).ToString());
+                StringBuilder sb = new StringBuilder();
+                foreach (string item in its)
+                {
+                    sb.Append("\n" + item.ToString());
+                }
+                richTextBox1.Text = sb.ToString();
+
+            }
+        }
+    }
+}
---
+++ C#-2/Lab4/FuncRange/FuncRange/NewParams.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FuncRange
+{
+    public partial class NewParams : Form
+    {
+        public NewParams()
+        {
+            InitializeComponent();
+        }
+        public double XFrom
+        { 
+            get {return double.Parse(textBoxFrom.Text);}
+            set { textBoxFrom.Text = value.ToString(); }
+        }
+
+        public double XTo
+        {
+            get { return double.Parse(textBoxTo.Text); }
+            set { textBoxTo.Text = value.ToString(); }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

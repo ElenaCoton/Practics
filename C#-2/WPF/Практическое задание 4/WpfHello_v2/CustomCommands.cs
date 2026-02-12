57cd9b5804c3fd61a74570504c91007bc585db1b
ElenaCoton <elkoton@mail.ru>
2026-02-12 09:03:31 +0300

    Add files via upload

---
+++ C#-2/WPF/Практическое задание 4/WpfHello_v2/CustomCommands.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfHello_v2
+{
+    public class CustomCommands
+    {
+        //Команду определить как статическое свойство только для чтения с именем Launch,
+        //возвращающую экземпляр RoutedUICommand:
+        public static RoutedUICommand Launch { get; }
+
+        //статический конструктор, который
+        //a.создает новую коллекцию InputGestureCollection,
+        //b. добавляет соответствующую клавишу ввода, связанную с новой командой (Ctrl+L)
+        //c.и инициирует переменную, возвращающую настраиваемую команду:
+        static CustomCommands()
+        {
+            InputGestureCollection myInputGestures = new
+            InputGestureCollection();
+            myInputGestures.Add(new KeyGesture(Key.L, ModifierKeys.Control));
+            Launch = new RoutedUICommand("Запуск", "Launch",  typeof(CustomCommands), myInputGestures);
+        }
+    }
+}

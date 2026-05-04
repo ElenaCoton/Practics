aaa191762d7554efc5e5dcb627db1ed4b247446a
Elena <elkoton@mail.ru>
2026-05-04 12:52:32 +0300

    Лаб работы 6-8

---
+++ ASP.NET/Lab6-8/MvcCreditApp/MvcCreditApp/Models/Bid.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcCreditApp.Models
+{
+    /// <summary>
+    /// модель данных о заявке на кредит
+    /// </summary>
+    public class Bid
+    {
+        // ID заявки
+        public virtual int BidId { get; set; }
+
+        // Имя заявителя
+        [DisplayName("Имя заявителя")]
+        [Required(ErrorMessage = "Поле обязательно")]
+        public virtual string Name { get; set; }
+
+        // Название кредита
+        [DisplayName("Название кредита")]
+        [Required(ErrorMessage = "Поле обязательно")]
+        public virtual string CreditHead { get; set; }
+
+        // Дата подачи заявки
+        [DisplayName("Дата подачи заявки")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yy}")]
+        [Required(ErrorMessage = "Поле обязательно")]
+        public virtual DateTime bidDate { get; set; }
+    }
+}
---
+++ ASP.NET/Lab6-8/MvcCreditApp/MvcCreditApp/Models/Credit.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcCreditApp.Models
+{
+    public class Credit
+    {
+        // ID кредита
+        public virtual int CreditId { get; set; }
+
+        // Название
+        [DisplayName("Название")]
+        [Required(ErrorMessage = "Поле обязательно")]
+        public virtual string Head { get; set; }
+
+        // Период, на который выдается кредит
+        [DisplayName("Период кредита")]
+        [Required(ErrorMessage = "Поле обязательно")]
+        public virtual int Period { get; set; }
+
+        // Максимальная сумма кредита
+        [DisplayName("MAX сумма кредита")]
+        [Required(ErrorMessage = "Поле обязательно")]
+        public virtual int Sum { get; set; }
+
+        // Процентная ставка
+        [DisplayName("Процентная ставка")]
+        [Required(ErrorMessage = "Поле обязательно")]
+        public virtual int Procent { get; set; }
+    }
+}
